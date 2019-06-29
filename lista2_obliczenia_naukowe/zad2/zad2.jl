4e6e99f80aecbc3cff42c4aa9f3d9f33fd1b80f0
Sernikjamnika <mati.walczak997@gmail.com>
2019-06-29 17:19:03 +0200

    Delete tmp directory

--- lista2_obliczenia_naukowe/zad2/zad2.jl
+++
@@ -1,8 +0,0 @@
-# author: Mateusz Walczak
-using SymPy
-
-
-# Sympy uses symbolic math
-# so it can calculate exact value of the limit
-x = Sym("x")
-println("Limit of e^x * log(1 + e^(-x) with x approaching inifinity is $(limit(e^x * log(1 + e^(-x)), x, oo))")
